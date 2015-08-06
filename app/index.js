968162d7e1f5b0455c18a0ef20a73ac02b11bca5
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-08-06 18:40:37 +0200

    move tests for es6

--- app/index.js
+++ app/index.js
@@ -1,14 +1,12 @@
-'use strict';
-
-var yeoman = require('yeoman-generator');
-var _s = require('underscore.string');
-var mkdirp = require('mkdirp');
-var normalizeUrl = require('normalize-url');
-var humanizeUrl = require('humanize-url');
+import yeoman from 'yeoman-generator';
+import _s from 'underscore.string';
+import mkdirp from 'mkdirp';
+import normalizeUrl from 'normalize-url';
+import humanizeUrl from 'humanize-url';
 
 module.exports = yeoman.generators.Base.extend({
-  init: function () {
-    var cb = this.async();
+  init() {
+    const cb = this.async();
 
     this.prompt([{
       name: 'isCorporate',
@@ -25,7 +23,7 @@ module.exports = yeoman.generators.Base.extend({
       name: 'moduleName',
       message: 'name:',
       default: this.appname.replace(/\s/g, '-'),
-      filter: function (val) { return _s.slugify(val); },
+      filter: val => _s.slugify(val),
     }, {
       name: 'moduleDesc',
       message: 'description:',
@@ -46,58 +44,50 @@ module.exports = yeoman.generators.Base.extend({
       name: 'github',
       message: 'your github:',
       store: true,
-      validate: function (val) {
-        return val.length > 0 ? true : 'You have to provide a username';
-      },
+      validate: val => (val.length > 0) ? true : 'You have to provide a username',
     }, {
       name: 'site',
       message: 'your site:',
       store: true,
-      filter: function (val) { return normalizeUrl(val); },
+      filter: val => normalizeUrl(val),
     }, {
-      when: function(props) { return props.isCorporate; },
+      when: props => props.isCorporate,
       name: 'companyName',
       message: 'company name:',
       store: true,
     }, {
-      when: function(props) { return props.isCorporate; },
+      when: props => props.isCorporate,
       name: 'companyGithub',
       message: 'company github:',
       store: true,
-      validate: function (val) {
-        return val.length > 0 ? true : 'You have to provide a username';
-      },
+      validate: val => (val.length > 0) ? true : 'You have to provide a username',
     }, {
-      when: function(props) { return props.isCorporate; },
+      when: props => props.isCorporate,
       name: 'companySite',
       message: 'company site:',
       store: true,
     }, {
-      when: function(props) { return !props.isOpensource; },
+      when: props => !props.isOpensource,
       name: 'repositoryUrl',
       message: 'repository url:',
-      validate: function (val) {
-        return val.length > 0 ? true : 'You have to repositoryUrl URL';
-      },
+      validate: val => (val.length > 0) ? true : 'You have to repositoryUrl URL',
     }, {
-      when: function(props) { return !props.isOpensource; },
+      when: props => !props.isOpensource,
       name: 'publishConfig',
       message: 'publishConfig:',
       store: true,
-      validate: function (val) {
-        return val.length > 0 ? true : 'You have to provide a publishConfig URL';
-      },
-    }], function (props) {
-      var name = this.user.git.name();
-      var email = this.user.git.email();
-      var moduleKeywords = (props.moduleKeywords || '').trim().split(',').map(function(i) { return (i || '').trim(); });
-      var licenseName = props.isCorporate ? props.companyName : name;
-      var licenseSite = props.isCorporate ? props.companySite : props.site;
-      var humanizedSite = props.site && humanizeUrl(props.site);
-      var humanizedCompanySite = (props.isCorporate && props.companySite) && humanizeUrl(props.companySite);
-      var humanizedLicenseSite = licenseSite && humanizeUrl(licenseSite);
+      validate: val => (val.length > 0) ? true : 'You have to provide a publishConfig URL',
+    }], props => {
+      const name = this.user.git.name();
+      const email = this.user.git.email();
+      const moduleKeywords = (props.moduleKeywords || '').trim().split(',').map(i => (i || '').trim());
+      const licenseName = props.isCorporate ? props.companyName : name;
+      const licenseSite = props.isCorporate ? props.companySite : props.site;
+      const humanizedSite = props.site && humanizeUrl(props.site);
+      const humanizedCompanySite = (props.isCorporate && props.companySite) && humanizeUrl(props.companySite);
+      const humanizedLicenseSite = licenseSite && humanizeUrl(licenseSite);
 
-      var tpl = {
+      const tpl = {
         moduleName: props.moduleName,
         camelModuleName: _s.camelize(props.moduleName),
         moduleDesc: props.moduleDesc,
@@ -121,9 +111,9 @@ module.exports = yeoman.generators.Base.extend({
         publishConfig: props.publishConfig,
       };
 
-      var cpTpl = function (from, to) {
+      const cpTpl = (from, to) => {
         this.fs.copyTpl(this.templatePath(from), this.destinationPath(to), tpl);
-      }.bind(this);
+      };
 
       mkdirp('src');
       mkdirp('test');
@@ -147,7 +137,7 @@ module.exports = yeoman.generators.Base.extend({
       cb();
     }.bind(this));
   },
-  install: function () {
+  install() {
     this.installDependencies({ bower: false });
-  }
+  },
 });
--- test/test.js
+++ test/test.js
@@ -1,79 +1,58 @@
-'use strict';
-var path = require('path');
-var helpers = require('yeoman-generator').test;
-var throws = require('assert').throws;
-var doesNotThrow = require('assert').doesNotThrow;
-var equal = require('assert').equal;
-var assert = require('yeoman-assert');
-var user = require('yeoman-generator/lib/actions/user').git;
-var fs = require('fs-extra');
+import path from 'path';
+import { test as helpers } from 'yeoman-generator';
+import { equal, doesNotThrow } from 'assert';
+import assert from 'yeoman-assert';
+import { git as user } from 'yeoman-generator/lib/actions/user';
+import fs from 'fs-extra';
 
-describe('generator', function() {
-  beforeEach(function(done) {
-    var deps = ['../../app'];
-
-    helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
+describe('generator', () => {
+  let ctx;
+  beforeEach(function beforeEachHook(done) {
+    ctx = this;
+    helpers.testDirectory(path.join(__dirname, 'temp'), err => {
       if (err) {
         done(err);
         return;
       }
-
-      this.generator = helpers.createGenerator('nordnet-es-module:app', deps, null, {skipInstall: true});
+      ctx.generator = helpers.createGenerator('nordnet-es-module:app', ['../../app'], null, { skipInstall: true });
       done();
-    }.bind(this));
+    });
   });
 
-  it('generates expected files', function(done) {
-
-    var expected = [
-      '.jscsrc',
-      '.eslintrc',
-      '.eslintignore',
-      '.editorconfig',
-      '.gitignore',
-      '.npmignore',
-      '.travis.yml',
-      '.istanbul.yml',
-      'package.json',
-      'README.md',
-      'src/index.js',
-      'test/index.test.js',
-      'test/mocha.opts',
-    ];
-
-    helpers.mockPrompt(this.generator, {
+  it('generates expected files', done => {
+    helpers.mockPrompt(ctx.generator, {
       moduleName: 'module',
-      moduleDesc: 'Your awsm module!'
+      moduleDesc: 'Your awsm module!',
     });
 
-    this.generator.run(function() {
-      assert.file(expected);
+    ctx.generator.run(() => {
+      assert.file([ '.jscsrc', '.eslintrc', '.eslintignore', '.editorconfig', '.gitignore', '.npmignore', '.travis.yml', '.istanbul.yml', 'package.json', 'README.md', 'src/index.js', 'test/index.test.js', 'test/mocha.opts' ]);
       done();
     });
   });
 
-  it('README corporate true without site', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('README corporate true without site', done => {
+    helpers.mockPrompt(ctx.generator, {
       isCorporate: true,
       moduleName: 'module',
-      companyName: 'Unicorn inc'
+      companyName: 'Unicorn inc',
     });
 
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.fileContent('README.md', /Unicorn inc/);
       done();
     });
   });
 
-  it('README corporate true with site', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('README corporate true with site', done => {
+    helpers.mockPrompt(ctx.generator, {
       isCorporate: true,
       moduleName: 'module',
       companyName: 'Unicorn inc',
       companySite: 'Unicorn.inc',
     });
 
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.fileContent('README.md', /Unicorn inc/);
       assert.fileContent('README.md', /Unicorn.inc/);
       done();
@@ -81,200 +60,198 @@ describe('generator', function() {
   });
 
 
-  it('README corporate false without site', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it.only('README corporate false without site', done => {
+    helpers.mockPrompt(ctx.generator, {
       isCorporate: false,
       moduleName: 'module',
     });
-
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.fileContent('README.md', user.name());
       done();
     });
   });
 
-  it('README corporate false with site', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('README corporate false with site', done => {
+    helpers.mockPrompt(ctx.generator, {
       isCorporate: false,
       moduleName: 'module',
-      site: 'asd'
+      site: 'asd',
     });
 
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.fileContent('README.md', user.name());
       done();
     });
   });
 
-  it('README opensource true', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('README opensource true', done => {
+    helpers.mockPrompt(ctx.generator, {
       isOpensource: true,
       moduleName: 'module',
     });
 
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.fileContent('README.md', /NPM version/);
       assert.fileContent('README.md', /npm-image/);
       done();
     });
   });
 
-  it('README opensource false', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('README opensource false', done => {
+    helpers.mockPrompt(ctx.generator, {
       isOpensource: false,
       moduleName: 'module',
     });
 
-    this.generator.run(function() {
+    ctx.generator.run(() => {
       assert.noFileContent('README.md', /NPM version/);
       assert.noFileContent('README.md', /npm-image/);
       done();
     });
   });
 
-  it('PKG validity, isCorporate true', function(done) {
-    helpers.mockPrompt(this.generator, { isCorporate: true });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
-        doesNotThrow(function() { if (err) throw err; }, /Unexpected token/);
+  it('PKG validity, isCorporate true', done => {
+    helpers.mockPrompt(ctx.generator, { isCorporate: true });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', err => {
+        doesNotThrow(() => { if (err) throw err; }, /Unexpected token/);
         done();
       });
     });
   });
 
-  it('PKG validity, isCorporate false', function(done) {
-    helpers.mockPrompt(this.generator, { isCorporate: false });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
-        doesNotThrow(function() { if (err) throw err; }, /Unexpected token/);
+  it('PKG validity, isCorporate false', done => {
+    helpers.mockPrompt(ctx.generator, { isCorporate: false });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', err => {
+        doesNotThrow(() => { if (err) throw err; }, /Unexpected token/);
         done();
       });
     });
   });
 
-  it('PKG validity, isOpensource true', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: true });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
-        doesNotThrow(function() { if (err) throw err; }, /Unexpected token/);
+  it('PKG validity, isOpensource true', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: true });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', err => {
+        doesNotThrow(() => { if (err) throw err; }, /Unexpected token/);
         done();
       });
     });
   });
 
-  it('PKG validity, isOpensource false', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: false });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
-        doesNotThrow(function() { if (err) throw err; }, /Unexpected token/);
+  it('PKG validity, isOpensource false', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: false });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', err => {
+        doesNotThrow(() => { if (err) throw err; }, /Unexpected token/);
         done();
       });
     });
   });
 
-  it('PKG crossplatform, isCorporate true', function(done) {
-    helpers.mockPrompt(this.generator, { isCorporate: true });
-    this.generator.run(function() {
+  it('PKG crossplatform, isCorporate true', done => {
+    helpers.mockPrompt(ctx.generator, { isCorporate: true });
+    ctx.generator.run(() => {
       assert.noFileContent('package.json', /&&/);
       done();
     });
   });
 
-  it('PKG crossplatform, isCorporate false', function(done) {
-    helpers.mockPrompt(this.generator, { isCorporate: false });
-    this.generator.run(function() {
+  it('PKG crossplatform, isCorporate false', done => {
+    helpers.mockPrompt(ctx.generator, { isCorporate: false });
+    ctx.generator.run(() => {
       assert.noFileContent('package.json', /&&/);
       done();
     });
   });
 
-  it('PKG crossplatform, isOpensource true', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: true });
-    this.generator.run(function() {
+  it('PKG crossplatform, isOpensource true', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: true });
+    ctx.generator.run(() => {
       assert.noFileContent('package.json', /&&/);
       done();
     });
   });
 
-  it('PKG crossplatform, isOpensource false', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: false });
-    this.generator.run(function() {
+  it('PKG crossplatform, isOpensource false', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: false });
+    ctx.generator.run(() => {
       assert.noFileContent('package.json', /&&/);
       done();
     });
   });
 
-  it('PKG publishConfig', function(done) {
-    helpers.mockPrompt(this.generator, {
+  it('PKG publishConfig', done => {
+    helpers.mockPrompt(ctx.generator, {
       isOpensource: false,
-      publishConfig: 'sinopia.ftw'
+      publishConfig: 'sinopia.ftw',
     });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(res.publishConfig.registry, 'sinopia.ftw');
         done();
-      })
+      });
     });
   });
 
-  it('PKG scripts, isOpensource true', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: true });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG scripts, isOpensource true', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: true });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(Object.keys(res.scripts).length, 12);
         done();
-      })
+      });
     });
   });
 
-  it('PKG scripts, isOpensource false', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: false });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG scripts, isOpensource false', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: false });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(Object.keys(res.scripts).length, 23);
         done();
-      })
+      });
     });
   });
 
-  it('PKG repository, isOpensource true', function(done) {
-    helpers.mockPrompt(this.generator, { github: 'nordnet' });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG repository, isOpensource true', done => {
+    helpers.mockPrompt(ctx.generator, { github: 'nordnet' });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(res.repository.url, 'git+https://github.com/nordnet/temp.git');
         done();
-      })
+      });
     });
   });
 
-  it('PKG repository, isOpensource false', function(done) {
-    helpers.mockPrompt(this.generator, { isOpensource: false, repositoryUrl: 'git+https://private.git' });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG repository, isOpensource false', done => {
+    helpers.mockPrompt(ctx.generator, { isOpensource: false, repositoryUrl: 'git+https://private.git' });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(res.repository.url, 'git+https://private.git');
         done();
-      })
+      });
     });
   });
 
-  it('PKG bugs', function(done) {
-    helpers.mockPrompt(this.generator, { github: 'nordnet' });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG bugs', done => {
+    helpers.mockPrompt(ctx.generator, { github: 'nordnet' });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(res.bugs.url, 'https://github.com/nordnet/temp/issues');
         done();
-      })
+      });
     });
   });
 
-  it('PKG homepage', function(done) {
-    helpers.mockPrompt(this.generator, { github: 'nordnet' });
-    this.generator.run(function() {
-      fs.readJson('./package.json', function(err, res) {
+  it('PKG homepage', done => {
+    helpers.mockPrompt(ctx.generator, { github: 'nordnet' });
+    ctx.generator.run(() => {
+      fs.readJson('./package.json', (err, res) => {
         equal(res.homepage, 'https://github.com/nordnet/temp#readme');
         done();
-      })
+      });
     });
   });
-
 });
